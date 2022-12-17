a51f9f769035834f761d16f86f70a6080f5dd9cf
hjun0917 <hjun0917@naver.com>
2022-12-17 20:17:42 +0900

    [chore] 프로젝트 환경 구성

---
+++ src/main/java/com/haelog/heaelogtestserver/HeaelogTestServerApplication.java
@@ -0,0 +1,13 @@
+package com.haelog.heaelogtestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeaelogTestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeaelogTestServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haelog/heaelogtestserver/HeaelogTestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haelog.heaelogtestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeaelogTestServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
