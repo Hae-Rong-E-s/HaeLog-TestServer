307399efbba1a67155a895b7e06740e72c2808de
hjun0917 <hjun0917@naver.com>
2022-12-18 17:50:17 +0900

    [chore] 환경 설정

---
+++ src/main/java/com/healog/haelogtestserver/HaeLogTestServerApplication.java
@@ -0,0 +1,13 @@
+package com.healog.haelogtestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaeLogTestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaeLogTestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healog/haelogtestserver/HaeLogTestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healog.haelogtestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaeLogTestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
